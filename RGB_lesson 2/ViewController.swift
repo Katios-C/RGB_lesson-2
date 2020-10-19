88becfd97760ff14cdee73b29c4f2d284b1e957c
Katios <kate.chonkina@gmail.com>
2020-10-19 11:26:32 +0600

    4

--- RGB_lesson 2/ViewController.swift
+++ RGB_lesson 2/ViewController.swift
@@ -10,12 +10,33 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var window: UIView!
-    
+    @IBOutlet var redText: UITextField!
     @IBOutlet var bluePercent: UILabel!
+    @IBOutlet var greenText: UITextField!
+    @IBOutlet var redPercent: UILabel!
+    @IBOutlet var blueText: UITextField!
+    @IBOutlet var greenPercent: UILabel!
     
+    @IBOutlet var sliderRed: UISlider!
+    
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        sliderRed.value = 0
+        sliderRed.minimumValue = 0
+        sliderRed.maximumValue = 1
+        sliderRed.thumbTintColor = .red
+        sliderRed.tintColor = .red
+        sliderGreen.tintColor = .green
+        sliderBlue.tintColor = .blue
+        
+        redPercent.text = String(sliderRed.value)
+        redPercent.font = redPercent.font.withSize(35)
+        
+        greenPercent.text = String(sliderGreen.value)
     }
 
 

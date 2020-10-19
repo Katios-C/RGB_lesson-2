98d7b2aa71c34603ec735a67d8f23e617abff846
Katios <kate.chonkina@gmail.com>
2020-10-19 10:13:33 +0600

    3

--- RGB_lesson 2/ViewController.swift
+++ RGB_lesson 2/ViewController.swift
@@ -9,11 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var window: UIView!
-    @IBOutlet var redPercent: UILabel!
-    @IBOutlet var bluePercent: UILabel!
-    @IBOutlet var greenPercent: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
